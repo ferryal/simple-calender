28da00d8aadd8cd83cdac5e1845593f1a2e63d5e
ferryal <ferryal.bahari@amartha.com>
2021-01-17 20:26:03 +0700

    add shared component form

---
+++ src/components/shared/forms/FormDatePicker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classNames from 'classnames';
+
+function FormDatePicker({
+  field,
+  form: { touched, errors },
+  className,
+  ...props
+}) {
+  const hasError = touched[field.name] && Boolean(errors[field.name]);
+
+  return (
+    <input
+      type="date"
+      className={classNames(
+        'placeholder-gray-400 bg-white border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm rounded-md',
+        {
+          'ring-2 ring-red-500': hasError,
+        },
+        className
+      )}
+      {...field}
+      {...props}
+    />
+  );
+}
+
+export default FormDatePicker;
